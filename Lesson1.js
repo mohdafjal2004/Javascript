6836093e452eb1a1b4608f20ad278ad9153c0fca
Mohd Afjal <mohdafjalgci2018@gmail.com>
2023-04-25 08:24:37 +0530

    First commit of js repo

---
+++ Lesson1.js
@@ -0,0 +1,17 @@
+console.log("Hello World");
+
+//Variables
+name = "Afjal";
+console.log(name);
+
+//User Input = prompt() for input and alert() is for output
+// fruit = prompt("What is your favourite fruit");
+// console.log(fruit);
+
+food = Number(prompt("Enter food quantity"));
+tipPercentage = Number(prompt("Enter Tip percentage") / 100);
+tipAmount = food * tipPercentage;
+
+TotalBill = food + tipAmount;
+console.log("total", TotalBill);
+// alert(tipAmount);x
