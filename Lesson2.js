bc6b07d3e63c73a076755911e4ecbd0f89d8fa52
Mohd Afjal <mohdafjalgci2018@gmail.com>
2023-04-25 10:29:36 +0530

    Conditional statements and conditonal operators

---
+++ Lesson2.js
@@ -0,0 +1,10 @@
+// Baby weather app (conditionals)
+//if rains 👉 "Grab your umbrell☂️"
+//else 👉 "Wear you sunglasse😎"
+
+let weather = prompt("How is the weather today");
+if (weather == "rainy") {
+  console.log("Grab your umbrella☂️");
+} else {
+  console.log("Wear you sunglasses😎");
+}
