f312721b6f1961107b0fefaafb223577c0eedf30
Mohd Afjal <mohdafjalgci2018@gmail.com>
2023-04-29 12:43:15 +0530

    Comments for easy understanding of functions in js

--- PROJECTS/Rock-Paper-Scissor/scriptGame.js
+++ PROJECTS/Rock-Paper-Scissor/scriptGame.js
@@ -4,11 +4,15 @@ const result = document.getElementById("result");
 const computerChoiceDiv = document.getElementById("computerChoice");
 const userChoiceDiv = document.getElementById("userChoice");
 
-//Declaring variables outside is very important
+//Declaring variables outside is very important,as it used while playing
+//Becoz it takes the choice from user and computer and then it updates the DOM
+//After updating the DOM we take the value and compare in getResult()
+
 let computerChoice;
 let userChoice;
 
-//Get the computer input as random number input
+//Get the computer input as random input
+// and it should only work on clicking the button in userPlay() function
 const computerPlay = () => {
   const random = Math.floor(Math.random() * buttons.length);
   computerChoice = buttons[random].innerText;
@@ -16,7 +20,10 @@ const computerPlay = () => {
   return computerChoice;
 };
 
-//user-choice
+//On clicking the button and updating the DOM with user choice,
+// then only we should get random choice from computer
+//from computerPlay() function and then only comparison should be made in getResult()
+//user-choice function
 const userPlay = () => {
   buttons.forEach(
     (btn) =>
@@ -30,6 +37,7 @@ const userPlay = () => {
 };
 let user = userPlay();
 
+//"let" Variables declared above will now be used for comparing and outputing the result
 const getResult = () => {
   if (userChoice === computerChoice) {
     result.innerHTML = " <h1>Result: Its a draw 🤷</h1>  <p>Keep Going</p>";
