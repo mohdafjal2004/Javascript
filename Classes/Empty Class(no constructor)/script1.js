fa74545ab930e49bea6ae3e8c3b79dc667206731
Mohd Afjal <mohdafjalgci2018@gmail.com>
2023-05-04 10:42:46 +0530

    Empty class with no constructor

---
+++ Classes/Empty Class(no constructor)/script1.js
@@ -0,0 +1,11 @@
+//Empty Class without constructor
+class Person {}
+
+//Here we have not defined constructor,
+//so compiler will automatically add an empty construtor called default constructor
+
+let person1 = new Person();
+person1.name = "Alice";
+person1.age = 30;
+console.log(person1.name);
+console.log(person1.age);
