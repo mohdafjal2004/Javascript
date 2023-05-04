b3ebcddcee89957da2fb102f6572e5c2c5fd4e1d
Mohd Afjal <mohdafjalgci2018@gmail.com>
2023-05-04 11:40:39 +0530

    Overridng default value of properties in object

--- Classes/Empty Class(no constructor)/WithProperties.js
+++ Classes/Empty Class(no constructor)/WithProperties.js
@@ -1,16 +1,17 @@
 //Class with no constructor but with  properties
+
 class Car {
-  name;
-  year;
-  color;
+  name = "Lambo";
+  year = "1999";
+  color = "red";
 }
 
 //Here also since we have not defined any constructor in class,
 //so compiler will add an empty constructor called default constructor
 let ferrari = new Car();
-ferrari.name = "Ferrari";
-ferrari.year = "1999";
-ferrari.color = "red";
+ferrari.name;
+ferrari.year;
+ferrari.color;
 console.log(ferrari.name);
 console.log(ferrari.year);
 console.log(ferrari.color);
