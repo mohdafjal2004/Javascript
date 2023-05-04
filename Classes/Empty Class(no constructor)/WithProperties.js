386108f343835576493f78a740141b86310eb6e6
Mohd Afjal <mohdafjalgci2018@gmail.com>
2023-05-04 11:43:07 +0530

    Simplied the concept using comment

--- Classes/Empty Class(no constructor)/WithProperties.js
+++ Classes/Empty Class(no constructor)/WithProperties.js
@@ -1,5 +1,6 @@
 //Class with no constructor but with  properties
-
+//All values the class are default (whether inside the constructor or outside the constructor)
+//So when creating the object we can use our own value of the property defined in the class
 class Car {
   name = "Lambo";
   year = "1999";
@@ -9,7 +10,8 @@ class Car {
 //Here also since we have not defined any constructor in class,
 //so compiler will add an empty constructor called default constructor
 let ferrari = new Car();
-ferrari.name;
+//Here the name is already declared, but still we can use our name
+ferrari.name="Ferrari";
 ferrari.year;
 ferrari.color;
 console.log(ferrari.name);
